e021cad635ab4bf8c4adfce81053d15d6a73e8e3
Tom <thunt8nrn@gmail.com>
2020-10-28 00:00:03 +0000

    Initial classes imported

---
+++ BombSquare.java
@@ -0,0 +1,15 @@
+public class BombSquare extends GameSquare
+{
+    private GameBoard board;                            // Object reference to the GameBoard this square is part of.
+    private boolean hasBomb;                            // True if this squre contains a bomb. False otherwise.
+
+	public static final int MINE_PROBABILITY = 10;
+
+	public BombSquare(int x, int y, GameBoard board)
+	{
+		super(x, y, "images/blank.png");
+
+        this.board = board;
+        this.hasBomb = ((int) (Math.random() * MINE_PROBABILITY)) == 0;
+    }    
+}
---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver
+{
+	public static void main(String[] Args)
+	{
+		GameBoard b = new GameBoard("BugSweeper", 30, 30, "BombSquare");
+	}
+}
